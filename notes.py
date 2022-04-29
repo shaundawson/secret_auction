3f3316052b9283f21c0250168220c04debb04f6e
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-29 00:50:26 -0400

    Update notes.py

--- notes.py
+++ notes.py
@@ -31,27 +31,63 @@
 
 
 #EXERCISE: GRADING PROGRAM EXERCISE
-student_scores = {
-  "Harry": 81,
-  "Ron": 78,
-  "Hermione": 99, 
-  "Draco": 74,
-  "Neville": 62,
-}
+# student_scores = {
+#   "Harry": 81,
+#   "Ron": 78,
+#   "Hermione": 99, 
+#   "Draco": 74,
+#   "Neville": 62,
+# }
 
 #Create an empty dictionary called student_grades.
-student_grades = {}
+# student_grades = {}
 
 #Add the grades to student_grades.
-for student in student_scores:
-    test_score = (student_scores[student])
-    if test_score >= 91:
-        student_grades[student] = "Outstanding"
-    elif test_score <= 90 and test_score >= 81:
-        student_grades[student]= "Exceeds Expectations"
-    elif test_score <= 80 and test_score >= 71:
-        student_grades[student] = "Acceptable"
-    else: 
-        student_grades[student] = "Fail"    
+# for student in student_scores:
+#     test_score = (student_scores[student])
+#     if test_score >= 91:
+#         student_grades[student] = "Outstanding"
+#     elif test_score <= 90 and test_score >= 81:
+#         student_grades[student]= "Exceeds Expectations"
+#     elif test_score <= 80 and test_score >= 71:
+#         student_grades[student] = "Acceptable"
+#     else: 
+#         student_grades[student] = "Fail"    
         
-print(student_grades)
+# print(student_grades)
+
+
+#NESTING
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+#Nesting list in a dictionary, Nesting dictionary in a dictonary
+travel_log = {
+    "France": {
+        "cities_visited":["Paris", "Lille", "Dijon"], 
+        "total_visits": 12
+},
+    "Germany":{
+        "cities_visited":["Berlin", "Hamburg", "Stuttgart"], 
+        "total_visits": 5
+              },
+}
+
+#Nesting a dictionary in a list
+travel_log = [
+  {
+    "country": "France",
+    "cities_visited": ["Paris", "Lille", "Dijon"], 
+    "total_visits": 12
+  },
+  {
+    "country": "Germany",
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], 
+    "total_visits":5
+  },   
+]
+
+
+
