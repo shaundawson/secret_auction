6a3ccc3bf38d1ed96eac65a089ee34c7ef1f3560
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-29 01:14:51 -0400

    exercise - travel log

--- notes.py
+++ notes.py
@@ -58,36 +58,58 @@
 
 
 #NESTING
-capitals = {
-    "France": "Paris",
-    "Germany": "Berlin",
-}
+# capitals = {
+#     "France": "Paris",
+#     "Germany": "Berlin",
+# }
 
 #Nesting list in a dictionary, Nesting dictionary in a dictonary
-travel_log = {
-    "France": {
-        "cities_visited":["Paris", "Lille", "Dijon"], 
-        "total_visits": 12
-},
-    "Germany":{
-        "cities_visited":["Berlin", "Hamburg", "Stuttgart"], 
-        "total_visits": 5
-              },
-}
+# travel_log = {
+#     "France": {
+#         "cities_visited":["Paris", "Lille", "Dijon"], 
+#         "total_visits": 12
+# },
+#     "Germany":{
+#         "cities_visited":["Berlin", "Hamburg", "Stuttgart"], 
+#         "total_visits": 5
+#               },
+# }
 
 #Nesting a dictionary in a list
+# travel_log = [
+#   {
+#     "country": "France",
+#     "cities_visited": ["Paris", "Lille", "Dijon"], 
+#     "total_visits": 12
+#   },
+#   {
+#     "country": "Germany",
+#     "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], 
+#     "total_visits":5
+#   },   
+# ]
+
+
+#EXERCISE - DICTIONARY IN LIST 
 travel_log = [
-  {
-    "country": "France",
-    "cities_visited": ["Paris", "Lille", "Dijon"], 
-    "total_visits": 12
-  },
-  {
-    "country": "Germany",
-    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], 
-    "total_visits":5
-  },   
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
 ]
 
-
-
+def add_new_country(country_visited,times_visited,cities_visited):
+    new_country = {}
+    new_country["country"] = country_visited
+    new_country["visits"] = times_visited
+    new_country["cities"] = cities_visited
+    travel_log.append(new_country)
+ 
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
