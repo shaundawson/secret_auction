ac45587312819b528fa38ddc9dc7328d7e57d8a4
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-28 23:46:07 -0400

    add new item to dictionary - notes

--- notes.py
+++ notes.py
@@ -3,8 +3,11 @@
 programming_dictionary = {
     "Bug":"An error in a program that prevents the program from running as expected.",
     "Function": "A piece of code that you can easily call over and over again.",
-    "Loop": "The action of doing something over and over again",
 }
 
 #Retrieve item from dictionary
 print(programming_dictionary["Bug"])
+
+#Adding new items to dictionary
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+print(programming_dictionary)
